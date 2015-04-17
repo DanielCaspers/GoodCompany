f364d26b1485aa961dce5d4bd34048f739d97fab
DanielCaspers <daniel@caspersonline.us>
2015-04-17 16:56:00 -0500

    Final Version

--- www/js/services.js
+++ www/js/services.js
@@ -14,84 +14,84 @@ angular.module('starter.services', [])
       }, {
         id: 1,
         name: 'Grouch O\'Callahan',
-        activities: ['Chess', 'Gardening', 'Rummy'],
+        activities: [ {name:'Chess', id:'5'}, {name:'Gardening', id:'10'}, {name:'Rummy', id:'22'}],
         since:'1427919070000',
         roomNumber: '303',
         image: 'img/ContactPhotos/elder2.jpg'
       }, {
         id: 2,
         name: 'Olga Finlayson',
-        activities: ['Reading', 'Drinking', 'Watching TV'],
+        activities: [ {name:'Reading', id:'21'}, {name:'Drinking', id:'8'}, {name:'Watching TV', id:'26'}],
         since:'1427141470000',
         roomNumber: '242',
         image: 'img/ContactPhotos/elder3.jpg'
       }, {
         id: 3,
         name: 'Marie Walton',
-        activities: ['Hiking', 'Biking', 'Travel'],
+        activities: [ {name:'Hiking', id:'12'}, {name:'Biking', id:'1'}, {name:'Travel', id:'24'}],
         since:'1425672670000',
         roomNumber: '117',
         image: 'img/ContactPhotos/elder4.jpg'
       }, {
         id: 4,
         name: 'Janet Miller',
-        activities: ['Poker', 'Bridge', 'Window Shopping'],
+        activities: [ {name:'Poker', id:'19'}, {name:'Bridge', id:'4'}, {name:'Window Shopping', id:'28'}],
         since:'1425240670000',
         roomNumber: '321',
         image: 'img/ContactPhotos/elder5.jpg'
       }, {
         id: 5,
         name: 'Perry Governor',
-        activities: ['Biking', 'Drinking', 'Museums'],
+        activities: [ {name:'Biking', id:'1'}, {name:'Drinking', id:'8'}, {name:'Museums', id:'15'}],
         since:'1425240670000',
         roomNumber: '237',
         image: 'img/ContactPhotos/elder6.jpg'
       }, {
         id: 6,
         name: 'Adam Bradleyson',
-        activities: ['History', 'Museums', 'Re-enactments', 'Theatre'],
+        activities: [ {name:'History', id:'12'}, {name:'Museums', id:'15'}, {name:'Re-enactments', id:'20'}],
         since:'1414008670000',
         roomNumber: '113',
         image: 'img/ContactPhotos/elder7.jpg'
       },{
         id: 7,
         name: 'Hoa Nguyen',
-        activities: ['Gardening', 'Sewing', 'Painting'],
+        activities: [ {name:'Gardening', id:'10'}, {name:'Sewing', id:'23'}, {name:'Painting', id:'16'}],
         since:'1405455070000',
         roomNumber: '204',
         image: 'img/ContactPhotos/elder8.jpg'
       },{
         id: 8,
         name: 'Mike November',
-        activities: ['Driving', 'Antiques', 'Smoking'],
+        activities: [ {name:'Driving', id:'9'}, {name:'Antiques', id:'0'}],
         since:'1395605470000',
         roomNumber: '309',
         image: 'img/ContactPhotos/elder9.jpg'
       },{
         id: 9,
         name: 'Ilene Killian',
-        activities: ['Reading', 'Book Clubs', 'Cooking'],
+        activities: [ {name:'Reading', id:'21'}, {name:'Book Clubs', id:'3'}, {name:'Cooking', id:'6'}],
         since:'1389989470000',
         roomNumber: '121',
         image: 'img/ContactPhotos/elder10.jpg'
       },{
         id: 10,
         name: 'Matilda Wexford',
-        activities: ['Smoking', 'Drinking', 'Bingo'],
+        activities: [ {name:'Drinking', id:'8'}, {name:'Bingo', id:'2'}],
         since:'1385928670000',
         roomNumber: '216',
         image: 'img/ContactPhotos/elder11.jpg'
       },{
         id: 11,
         name: 'Bob Stout',
-        activities: ['Whittling', 'Piano', 'Book Clubs'],
+        activities: [ {name:'Whittling', id:'27'}, {name:'Piano', id:'18'}, {name:'Book Clubs', id:'3'}],
         since:'1378732370000',
         roomNumber: '100',
         image: 'img/ContactPhotos/elder12.jpg'
       },{
         id: 12,
         name: 'Walter Pollak',
-        activities: ['Walking', 'Parks', 'Poker'],
+        activities: [ {name:'Walking', id:'25'}, {name:'Parks', id:'17'}, {name:'Poker', id:'19'}],
         since:'1376511070000',
         roomNumber: '305',
         image: 'img/ContactPhotos/elder13.jpg'
@@ -135,120 +135,144 @@ angular.module('starter.services', [])
       }, {
         id: 1,
         name: 'Biking',
-        people: [{name:'Marie Walton', id:'2', image:'img/ContactPhotos/elder3.jpg'}, {name:'Perry Governor', id:'5', image:'img/ContactPhotos/elder6.jpg'},
-          {name:'Marie Walton', id:'2', image:'img/ContactPhotos/elder3.jpg'}
-        ],
+        people: [{name:'Marie Walton', id:'2', image:'img/ContactPhotos/elder3.jpg'}, {name:'Perry Governor', id:'5', image:'img/ContactPhotos/elder6.jpg'}],
         image: 'img/activities/biking.jpg'
       }, {
         id: 2,
         name: 'Bingo',
-        image: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+        people: [{name:'Ben Sparrow', id:'0', image:'img/ContactPhotos/elder1.jpg'}, {name:'Matilda Wexford', id:'10', image:'img/ContactPhotos/elder11.jpg'}],
+        image: 'img/activities/bingo.jpg'
       }, {
         id: 3,
         name: 'Book Clubs',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Ilene Killian', id:'9', image:'img/ContactPhotos/elder10.jpg'}, {name:'Bob Stout', id: '11', image:'img/ContactPhotos/elder11.jpg'}],
+        image: 'img/activities/bookclub.jpg'
       }, {
         id: 4,
         name: 'Bridge',
-        image: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+        people: [{name:'Janet Miller', id:'4', image:'img/ContactPhotos/elder5.jpg'}],
+        image: 'img/activities/bridge.jpg'
       },{
         id: 5,
         name: 'Chess',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Grouch O\'Callahan', id:'1', image:'img/ContactPhotos/elder2.jpg'}],
+        image: 'img/activities/chess.jpg'
       },{
         id: 6,
         name: 'Cooking',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Ilene Killian', id:'9', image:'img/ContactPhotos/elder10.jpg'}],
+        image: 'img/activities/cooking.jpg'
       },{
         id: 7,
         name: 'Dominoes',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        image: 'img/activities/dominoes.jpg'
       },{
         id: 8,
         name: 'Drinking',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Olga Finlayson', id:'2', image:'img/ContactPhotos/elder3.jpg'}, {name:'Perry Governor', id: '5', image:'img/ContactPhotos/elder6.jpg'}, {name:'Matilda Wexford', id: '10', image:'img/ContactPhotos/elder11.jpg'}],
+        image: 'img/activities/drinking.jpg'
       },{
         id: 9,
         name: 'Driving',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Mike November', id:'9', image:'img/ContactPhotos/elder10.jpg'}],
+        image: 'img/activities/driving.jpg'
       },{
         id: 10,
         name: 'Gardening',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Grouch O\'Callahan', id:'1', image:'img/ContactPhotos/elder2.jpg'}, {name:'Hoa Nguyen', id:'7', image:'img/ContactPhotos/elder8.jpg'}],
+        image: 'img/activities/gardening.jpg'
       },{
         id: 11,
         name: 'Golf',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Ben Sparrow', id:'0', image:'img/ContactPhotos/elder1.jpg'}],
+        image: 'img/activities/golf.jpg'
       },{
         id: 12,
         name: 'Hiking',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Marie Walton', id:'3', image:'img/ContactPhotos/elder4.jpg'}],
+        image: 'img/activities/hiking.jpg'
       },{
         id: 13,
         name: 'History',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Adam Bradleyson', id:'6', image:'img/ContactPhotos/elder7.jpg'}],
+        image: 'img/activities/history.jpg'
       },{
         id: 14,
         name: 'Horseshoes',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Ben Sparrow', id:'0', image:'img/ContactPhotos/elder1.jpg'}],
+        image: 'img/activities/horseshoe.jpeg'
       },{
         id: 15,
         name: 'Museums',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Perry Governor', id:'5', image:'img/ContactPhotos/elder6.jpg'}, {name:'Adam Bradleyson', id:'6', image:'img/ContactPhotos/elder7.jpg'}],
+        image: 'img/activities/museum.jpg'
       },{
         id: 16,
         name: 'Painting',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Hoa Nguyen', id:'7', image:'img/ContactPhotos/elder8.jpg'}],
+        image: 'img/activities/painting.jpg'
       },{
         id: 17,
         name: 'Parks',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Walter Pollak', id:'12', image:'img/ContactPhotos/elder13.jpg'}],
+        image: 'img/activities/parks.jpg'
       },{
         id: 18,
         name: 'Piano',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Bob Stout', id:'11', image:'img/ContactPhotos/elder12.jpg'}],
+        image: 'img/activities/piano.jpg'
       },{
         id: 19,
         name: 'Poker',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Janet Miller', id:'4', image:'img/ContactPhotos/elder5.jpg'}, {name:'Walter Pollak', id:'12', image:'img/ContactPhotos/elder13.jpg'}],
+        image: 'img/activities/poker.jpg'
       },{
         id: 20,
         name: 'Re-enactments',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Adam Bradleyson', id:'6', image:'img/ContactPhotos/elder7.jpg'}],
+        image: 'img/activities/reenactment.jpg'
       },{
         id: 21,
         name: 'Reading',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Olga Finlayson', id:'2', image:'img/ContactPhotos/elder3.jpg'}, {name:'Ilene Killian', id:'9', image:'img/ContactPhotos/elder10.jpg'}],
+        image: 'img/activities/reading.jpg'
       },{
         id: 22,
         name: 'Rummy',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Grouch O\'Callahan', id:'1', image:'img/ContactPhotos/elder2.jpg'}],
+        image: 'img/activities/rummy.jpg'
       },{
         id: 23,
         name: 'Sewing',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Hoa Nguyen', id:'7', image:'img/ContactPhotos/elder8.jpg'}],
+        image: 'img/activities/sewing.jpg'
       },{
         id: 24,
         name: 'Travel',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Marie Walton', id:'3', image:'img/ContactPhotos/elder4.jpg'}],
+        image: 'img/activities/travel.jpg'
       },{
         id: 25,
         name: 'Walking',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Walter Pollak', id:'12', image:'img/ContactPhotos/elder13.jpg'}],
+        image: 'img/activities/walking.jpg'
       },{
         id: 26,
         name: 'Watching TV',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Olga Finlayson', id:'2', image:'img/ContactPhotos/elder3.jpg'}],
+        image: 'img/activities/watchingtv.jpg'
       },{
         id: 27,
         name: 'Whittling',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Bob Stout', id:'11', image:'img/ContactPhotos/elder13.jpg'}],
+        image: 'img/activities/whittling.jpg'
       },{
         id: 28,
         name: 'Window Shopping',
-        image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        people: [{name:'Janet Miller', id:'4', image:'img/ContactPhotos/elder5.jpg'}],
+        image: 'img/activities/windowshopping.jpg'
       }];
-      
+
       return {
         all: function() {
           return activities;
